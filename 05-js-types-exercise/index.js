299c75c0e66a84913c225e477ee8ce90826a1cc3
Mohammed Khalif <mkhalif@live.co.uk>
2020-03-04 16:06:21 +0000

    commit

---
+++ 05-js-types-exercise/index.js
@@ -0,0 +1,25 @@
+
+
+const greeting = "Hello";
+const name = "Mimi!";
+
+const phrase = `greeting: ${greeting} and name: ${name}`;
+
+const price = 10.99;
+const quantity = 43;
+
+const total = price * quantity;
+
+const age1 = 10;
+const age2 = 5;
+const age3 = 12;
+
+const averageAge = (age1 + age2 + age3) / 3;
+
+const loggedIn = true;
+const adminUser = false;
+
+let missingInfo = undefined;
+
+
+
