288f1fc1ec51e3ddd449697c0f1a0621e1fd8aa7
Mohammed Khalif <mkhalif@live.co.uk>
2020-04-06 13:38:48 +0100

    commit

--- 38-DOM-selecting-elements/index.js
+++ 38-DOM-selecting-elements/index.js
@@ -5,11 +5,15 @@
  * to variable {paragraphs}
  */
 
+const paragraphs = document.getElementsByTagName("p");
+
 /**
  * Exercise 2
  * select the first p tag with getElementsByTagName and assign it
  * to variable {firstParagraph}
  */
+
+const firstParagraph = document.getElementsByTagName("p")[0]; 
 
 /**
  * Exercise 3
@@ -20,12 +24,22 @@
  * ATTENTION: check the type of {paragraphs} before doing forEach
  */
 
+function logAllParagraphs(){
+    for (let index = 0; index < paragraphs.length; index++) {
+        console.log(paragraphs[index]);
+        
+    }
+}
+
 /**
  * Exercise 4
  *
  * select all elements with class "text" with getElementsByClassName
  * and assign it to variable {textElements}
  */
+
+
+const textElements = document.getElementsByClassName("text");
 
 /**
  * Exercise 5
@@ -34,12 +48,17 @@
  * with getElementsByClassName and assign it to variable {greenText}
  */
 
+const greenText = document.getElementsByClassName("text green");
+
 /**
  * Exercise 6
  *
- * select element where id is 'textId-3'(use getElementById) and assign it
+ * select element where id is 'kitten' (use getElementById) and assign it
  * to variable {thirdText}
  */
+
+const thirdText = document.getElementById("kitten");
+
 
 /* ==================================================== */
 /*                  querySelector section               */
@@ -52,17 +71,23 @@
  * select the fourth "li" element and save it to {fourthListElement} variable
  */
 
+const fourthListElement = document.querySelector("li:nth-child(4)");
+
 /**
  * Exercise 8
  *
  * select all "li" elements and save it to {listElements} variable
  */
 
+const listElements = document.querySelectorAll("li");
+
 /**
  * Exercise 8
  *
  * select all "a" elements that are inside "ul" and assign it {listLinks}
  */
+
+const listLinks = document.querySelectorAll("ul > a");
 
 /**
  * Exercise 9
@@ -71,12 +96,16 @@
  * is "codersinhoods" and assign it {communityElements}
  */
 
+const communityElements = document.querySelectorAll('[data-community-name = "codersinhoods"]');
+
 /**
  * Exercise 10
  *
  * select all elements with class "test" and assign it to a variable
  * {elementsForTesting}
  */
+
+const elementsForTesting = document.querySelectorAll('[class = "test"]');
 
 /**
  * Exercise 11
@@ -85,9 +114,13 @@
  * {fifthTest}
  */
 
+const fifthTest = document.querySelector('li[id = "test-5"]');
+
 /**
  * Exercise 12
  *
  * select all the elements which are siblings of an element
  * with the class "links" and assign it to a variable {nextElements}
  */
+
+const nextElements = document.querySelectorAll('.links ~ a');
